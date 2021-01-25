c3dcd64c9689bb15b332257de7be7ee5d8cdb77a
Effiongoto <effiongoto98@gmail.com>
2021-01-25 09:35:22 +0100

    Prepare for deployment3

--- backend/server.js
+++ backend/server.js
@@ -33,17 +33,17 @@ app.get('/api/config/paystack', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-// if (process.env.NODE_ENV !== 'development') {
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+if (process.env.NODE_ENV !== 'development') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-);
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....');
-//   });
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
